5333a0c869ba06f33ca828c9fb6ba3674405d303
Daniel-Steven-Holguin-Soto <holguinsotodanielsteven@gmail.com>
2025-11-05 20:19:49 -0500

    feat: componente Experiencia con 4 cargos definidos

---
+++ exp_app/src/componentes/Experiencia.jsx
@@ -0,0 +1,18 @@
+export default function Contact() {
+  return (
+    <section>
+      <h2>Desarrollador Front-End Jr. — TechNova Solutions (2025)</h2>
+      <p>Participé en la creación de una página web corporativa utilizando React y Vite, implementando componentes reutilizables y optimizando el rendimiento. Colaboré con el equipo de diseño para asegurar una experiencia de usuario fluida y moderna.
+      </p>
+
+      <h2>Asistente de Desarrollo Web — CodeFactory (2024)</h2>
+      <p>Apoyé en la maquetación de sitios web con HTML, CSS y JavaScript. Realicé pruebas de compatibilidad entre navegadores y ayudé a mejorar la accesibilidad del contenido.</p>
+
+      <h2>Proyecto Académico: “Gestor de Tareas” — SENA (2024)</h2>
+      <p>Desarrollé una aplicación web para gestionar tareas diarias, empleando React para el manejo de estados y componentes. Implementé un diseño responsivo con CSS y funciones básicas de CRUD.</p>
+
+      <h2>Prácticas en InnovApp — (2023)</h2>
+      <p>Colaboré en el desarrollo de una aplicación web de portafolio personal, utilizando Vite como herramienta de compilación y GitHub para control de versiones. Aprendí sobre organización de código y despliegue en entornos web.</p>
+    </section>
+);
+}
