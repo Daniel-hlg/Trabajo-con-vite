e6b2deaca5557bf4230cbd8e173d2d3190ba5470
Daniel-Steven-Holguin-Soto <holguinsotodanielsteven@gmail.com>
2025-11-05 20:16:46 -0500

    feat: componente CabeceraCV con datos personales actualizados

---
+++ exp_app/src/componentes/CabeceraCV.jsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>Daniel Steven Holguín Soto</h1>
+      <p>Desarrollador Front-End Jr.</p>
+
+      <h2>Contacto</h2>
+        <ul>
+          <li>holguinsotodanielsteven@gmail.com</li>
+          <li>3127927098</li>
+          <li>T.I 1020115756</li>
+          <li>Colombia - Medellín</li>
+        </ul>
+    </header>
+);
+}
