d6a5ab909edf29254e78fe533caf43b8278170a6
Daniel-Steven-Holguin-Soto <holguinsotodanielsteven@gmail.com>
2025-11-13 08:05:46 -0500

    refactor: extracción de datos del CV en archivo cvData.js

---
+++ exp_app/src/componentes/cvData.js
@@ -0,0 +1,23 @@
+export const cvData = {
+  nombre: "Daniel Steven Holguín Soto",
+  cargo: "Aprendiz de Análisis y Desarrollo de Software",
+  ciudad: "Medellín",
+  contacto: "holguinsotodanielsteven@gmail.com",
+
+  perfil: "Soy un aprendiz del SENA en el programa de Análisis y Desarrollo de Software (ADSO). Me interesa el desarrollo web y busco fortalecer mis conocimientos en React, Node.js y bases de datos.",
+
+  experiencia: [
+    { cargo: "Desarrollador Front-End", empresa: "SENA", año: 2025, descripcion: "Creación de componentes en React." },
+    { cargo: "Diseñador UI", empresa: "SENA", año: 2025, descripcion: "Diseño de interfaces con Figma." },
+    { cargo: "Soporte Técnico", empresa: "SENA", año: 2025, descripcion: "Mantenimiento y asistencia técnica." },
+  ],
+
+  educacion: [
+    { institucion: "I.E Alberto Diaz Muñoz", curso: "Bachiller Académico", año: 2024 },
+    { institucion: "SENA - CTMA", curso: "Tecnólogo en ADSO", año: "2025 - 2027" },
+    { institucion: "SENA", curso: "Inglés Nivel 1 y 2", año: "2025"},
+  ],
+
+  tecnologias: ["JavaScript", "HTML", "CSS", "ControlDeVersiones", "Vite"]
+};
+
