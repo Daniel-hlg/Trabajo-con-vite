70763911968f5cb95c486e783a4dd72953add024
Daniel-Steven-Holguin-Soto <holguinsotodanielsteven@gmail.com>
2025-11-13 07:54:56 -0500

    feact: componente Educacion con props y desestructuracion

--- exp_app/src/componentes/Educacion.jsx
+++ exp_app/src/componentes/Educacion.jsx
@@ -1,40 +1,17 @@
-function Educacion() {
-  const educacion = [
-    {
-      institucion: "I.E Alberto Diaz Muñoz",
-      curso: "Bachiller Académico",
-      año: 2024,
-    },
-    {
-      institucion: "SENA - Centro de Tecnología de la Manufactura Avanzada (CTMA)",
-      curso: "Tecnólogo en Análisis y Desarrollo de Software (ADSO)",
-      año: "2025 - 2027",
-    },
-    {
-      institucion: "SENA",
-      curso: "Inglés Nivel 1",
-      año: 2025,
-    },
-    {
-      institucion: "SENA",
-      curso: "Inglés Nivel 2",
-      año: 2025,
-    },
-  ];
+import React from "react";
 
+function Educacion({ estudios }) {
   return (
-    <div>
-      <section>
-        <h2>Educación</h2>
-        <ul>
-          {educacion.map((estudio, index) => (
-            <li key={index}>
-              <strong>{estudio.curso}</strong> - {estudio.institucion} ({estudio.año})
-            </li>
-          ))}
+    <section>
+      <h1>Educación</h1>
+      <ul>
+        {estudios.map((educacion, index) => (
+          <li key={index}>
+            <strong>{educacion.curso}</strong> - {educacion.institucion} ({educacion.año})
+          </li>
+        ))}
       </ul>
-      </section>
-    </div>
+    </section>
   );
 }
 
