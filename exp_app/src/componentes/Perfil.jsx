b1502893e897d40094bb4aae38cde391434704c5
Daniel-Steven-Holguin-Soto <holguinsotodanielsteven@gmail.com>
2025-11-13 07:57:43 -0500

    feat: componente Perfil dinamico con props

--- exp_app/src/componentes/Perfil.jsx
+++ exp_app/src/componentes/Perfil.jsx
@@ -1,18 +1,12 @@
-export default function About() {
+import React from "react";
+
+function Perfil({yo}) {
   return (
     <section>
-      <h2>Sobre mí</h2>
-      <p>
-        Soy un aprendiz de desarrollo Front-End con interés en crear interfaces web dinámicas y funcionales utilizando tecnologías modernas como React y Vite. Me caracterizo por mi disposición para aprender, adaptarme a nuevas herramientas y trabajar en equipo. Durante mi formación he adquirido conocimientos sobre la estructura de proyectos, el uso de componentes y la comprensión del flujo de datos en aplicaciones web.
-      </p>
-
-      <h2>Habilidades</h2>
-        <ul>
-          <li>HTML (Tailwind)</li>
-          <li>SQL y MySQL</li>
-          <li>Control de versiones</li>
-          <li>CSS</li>
-        </ul>
+      <h1>Perfil</h1>
+      <p>{yo}</p>
     </section>
-);
+  );
 }
+
+export default Perfil;
