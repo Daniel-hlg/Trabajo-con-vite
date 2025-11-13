f4810f59d847bcb8ec2f7b31992c9c7d3f0f22ea
Daniel-Steven-Holguin-Soto <holguinsotodanielsteven@gmail.com>
2025-11-13 08:01:54 -0500

    refactor: extraccion de datos personales en App.jsx

--- exp_app/src/componentes/App.jsx
+++ exp_app/src/componentes/App.jsx
@@ -4,17 +4,28 @@ import Educacion from "./Educacion";
 import Experiencia from "./Experiencia";
 import Foto from "./Foto";
 import StackTecnologias from "./StackTecnologias";
+import {cvData} from "./cvData"
 import "./App.css";
 
 export default function App() {
   return (
     <div className="container">
-      <Cabecera />
-      <Perfil />
-      <Educacion />
-      <Experiencia />
-      <StackTecnologias />
-      <Foto />
+      <Cabecera
+      nombre={cvData.nombre}
+      cargo={cvData.cargo}
+      ciudad={cvData.ciudad}
+      contacto={cvData.contacto}
+      />
+
+      <Perfil yo={cvData.perfil} />
+
+      <Experiencia experiencias={cvData.experiencia} />
+
+      <Educacion estudios={cvData.educacion} />
+
+      <StackTecnologias tecnologias={cvData.tecnologias} />
+
+      <Foto foto={cvData.foto} />
     </div>
-);
+  );
 }
