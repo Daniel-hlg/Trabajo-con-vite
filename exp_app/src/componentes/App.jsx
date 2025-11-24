3a10c7808a800c1fc2af875d87e4e4fe96787e8d
Daniel-Steven-Holguin-Soto <holguinsotodanielsteven@gmail.com>
2025-11-24 11:36:04 -0500

    feat: función agregarTecnologia en App.jsx y paso como prop

--- exp_app/src/componentes/App.jsx
+++ exp_app/src/componentes/App.jsx
@@ -1,20 +1,31 @@
+import { useState } from "react";
 import Cabecera from "./CabeceraCV";
 import Perfil from "./Perfil";
 import Educacion from "./Educacion";
 import Experiencia from "./Experiencia";
 import Foto from "./Foto";
 import StackTecnologias from "./StackTecnologias";
-import {cvData} from "./cvData"
+import ToggleHabilidades from "./ToggleHabilidades";
+import FormularioTecnologia from "./FormularioTecnologia"; 
+import { cvData } from "./cvData";
 import "./App.css";
 
 export default function App() {
+
+  const [tecnologias, setTecnologias] = useState(cvData.tecnologias);
+
+  const agregarTecnologia = (nueva) => {
+    setTecnologias([...tecnologias, nueva]);
+  };
+
   return (
     <div className="container">
+
       <Cabecera
-      nombre={cvData.nombre}
-      cargo={cvData.cargo}
-      ciudad={cvData.ciudad}
-      contacto={cvData.contacto}
+        nombre={cvData.nombre}
+        cargo={cvData.cargo}
+        ciudad={cvData.ciudad}
+        contacto={cvData.contacto}
       />
 
       <Perfil yo={cvData.perfil} />
@@ -23,7 +34,11 @@ export default function App() {
 
       <Educacion estudios={cvData.educacion} />
 
-      <StackTecnologias tecnologias={cvData.tecnologias} />
+      <StackTecnologias tecnologias={tecnologias} />
+
+      <ToggleHabilidades />
+
+      <FormularioTecnologia agregarTecnologia={agregarTecnologia} />
 
       <Foto foto={cvData.foto} />
     </div>
