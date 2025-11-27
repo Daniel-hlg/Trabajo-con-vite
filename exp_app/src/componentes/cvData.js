8f9bf1fd5b50cc406f7af0d112a616518b56b078
Daniel-Steven-Holguin-Soto <holguinsotodanielsteven@gmail.com>
2025-11-27 16:27:40 -0500

    feat: implementación de useState para mostrar/ocultar habilidades

--- exp_app/src/componentes/cvData.js
+++ exp_app/src/componentes/cvData.js
@@ -4,20 +4,54 @@ export const cvData = {
   ciudad: "Medellín",
   contacto: "holguinsotodanielsteven@gmail.com",
 
-  perfil: "Soy un aprendiz del SENA en el programa de Análisis y Desarrollo de Software (ADSO). Me interesa el desarrollo web y busco fortalecer mis conocimientos en React, Node.js y bases de datos.",
+  perfil:
+    "Soy un aprendiz del SENA en el programa de Análisis y Desarrollo de Software (ADSO). Me interesa el desarrollo web y busco fortalecer mis conocimientos en React, Node.js y bases de datos.",
 
   experiencia: [
-    { cargo: "Desarrollador Front-End", empresa: "SENA", año: 2025, descripcion: "Creación de componentes en React." },
-    { cargo: "Diseñador UI", empresa: "SENA", año: 2025, descripcion: "Diseño de interfaces con Figma." },
-    { cargo: "Soporte Técnico", empresa: "SENA", año: 2025, descripcion: "Mantenimiento y asistencia técnica." },
+    {
+      cargo: "Desarrollador Front-End",
+      empresa: "SENA",
+      año: 2025,
+      descripcion: "Creación de componentes en React.",
+    },
+    {
+      cargo: "Diseñador UI",
+      empresa: "SENA",
+      año: 2025,
+      descripcion: "Diseño de interfaces con Figma.",
+    },
+    {
+      cargo: "Soporte Técnico",
+      empresa: "SENA",
+      año: 2025,
+      descripcion: "Mantenimiento y asistencia técnica.",
+    },
   ],
 
   educacion: [
-    { institucion: "I.E Alberto Diaz Muñoz", curso: "Bachiller Académico", año: 2024 },
-    { institucion: "SENA - CTMA", curso: "Tecnólogo en ADSO", año: "2025 - 2027" },
-    { institucion: "SENA", curso: "Inglés Nivel 1 y 2", año: "2025"},
+    {
+      institucion: "I.E Alberto Diaz Muñoz",
+      curso: "Bachiller Académico",
+      año: 2024,
+    },
+    {
+      institucion: "SENA - CTMA",
+      curso: "Tecnólogo en ADSO",
+      año: "2025 - 2027",
+    },
+    {
+      institucion: "SENA",
+      curso: "Inglés Nivel 1 y 2",
+      año: "2025",
+    },
   ],
 
-  tecnologias: ["JavaScript", "HTML", "CSS", "ControlDeVersiones", "Vite"]
-};
+  tecnologias: ["JavaScript", "HTML", "CSS", "ControlDeVersiones", "Vite"],
 
+  habilidades: [
+    "Trabajo en equipo",
+    "Comunicación efectiva",
+    "Responsabilidad",
+    "Pensamiento crítico",
+  ],
+};
